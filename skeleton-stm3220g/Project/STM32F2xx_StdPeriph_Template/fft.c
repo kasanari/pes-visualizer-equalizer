55f0b1e327326f385c441b03d2919029890a8b5d
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-04-09 19:05:01 +0200

    custom fft code

--- skeleton-stm3220g/Project/STM32F2xx_StdPeriph_Template/fft.c
+++ skeleton-stm3220g/Project/STM32F2xx_StdPeriph_Template/fft.c
@@ -1,4 +1,4 @@
-#include "arm_math.h"
+//#include "arm_math.h"
 #include "stm32f2xx_adc.h"
 #include "stm32_audio.h"
 #include "stm322xg_audio_recorder.h"
@@ -7,10 +7,13 @@
 #include "fft.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define FFT_LENGTH 256 				/* number of frequency bins/segments */
 
-#define DEFAULT_VOLUME  70    /* Default volume in % (Mute=0%, Max = 100%) in Logarithmic values      */                                        
+#define PI 3.141592653589793238462643383279502884197169399
+
+//#define DEFAULT_VOLUME  70    /* Default volume in % (Mute=0%, Max = 100%) in Logarithmic values      */                                        
 																									
 xSemaphoreHandle buffer_switch_sem;
 
@@ -32,34 +35,104 @@ uint32_t STM32_AudioRec_Update(uint8_t** pbuf, uint32_t* pSize) {
 	return 0;
 }
 
-static void transformTask(void *params) {
-	static arm_rfft_instance_q15 fft_instance;
-	arm_status status;
-	
-	uint32_t inverse_fft = 0; /* use forward FFT */
-	uint32_t bit_reverse_flag = 1; /* use normal bit order for output */
-	
-	static q15_t output[FFT_LENGTH*2]; //has to be twice FFT size because of symmetry
-	
-	audio_buffer_1 = calloc(FFT_LENGTH, sizeof(uint16_t));
-	audio_buffer_2 = calloc(FFT_LENGTH, sizeof(uint16_t));
-	
-	STM32_AudioRec_Init(SAMPLE_RATE_44100, DEFAULT_IN_BIT_RESOLUTION, DEFAULT_IN_CHANNEL_NBR);
-	STM32_AudioRec_Start((uint8_t*)audio_buffer_1, FFT_LENGTH);
-	
-	status = arm_rfft_init_q15(&fft_instance, FFT_LENGTH, inverse_fft, bit_reverse_flag);
-	printf("FFT init status: %d\n", status);
-	
-	for (;;) {
+/*Reverse a number of bits*/
+static uint16_t reverse_bits(uint16_t bits, uint16_t n_bits) {
+	uint16_t bits_reversed = 0; 		// To store the reversed bits
+	uint16_t i;
+	for (i = 0; i < n_bits; i++) {
+		bits_reversed <<= 1; 					// Shift the result to the left
+		bits_reversed |= (bits & 1U); // Add the current last bit of bits.
+		bits >>= 1; 									// Shift the original bits to the right
+	}
+	return bits_reversed;
+}
 
-		arm_rfft_q15(&fft_instance, (q15_t*)audio_buffer_1, output); /* perform FFT */
+
+uint16_t* FFT(uint16_t *samples, uint16_t fft_length) {
+	uint8_t levels = 0;
+	uint8_t i, j, k;
+	double w_m_r, w_m_i, w_i, w_r, t_r, t_i, u_i, u_r;
+	uint8_t rev_index;
+	uint8_t m;
+	volatile double A_real[4] = {0, 0, 0, 0};
+	volatile double A_imag[4] = {0, 0, 0, 0};
+	double w_r_temp;
+	uint16_t input[4] = {2, 1, 0, 1};
+	
+	for (i = fft_length; i > 1; i >>= 1) { // Take the two-logarithm of fft_length to determine number of steps
+		levels++;
+	}
+	
+	if ((1U << levels) != fft_length) { // Verify that fft_length is an exponential of 2
+		return NULL;
+	}
+	
+	for (i = 0; i < fft_length; i++) {
+		rev_index = reverse_bits(i, levels);
+		A_real[rev_index] = input[i];
+	}
+	
+	for (i = 0; i <= levels; i++) {
+		m = 1 << i;
 		
-		arm_abs_q15(output, output, FFT_LENGTH);
-		vTaskDelay(300 / portTICK_RATE_MS);
+		// w_m = cos((2*PI)/m) - i*sin((2*PI)/m)
+		w_m_r = cos((2*PI)/m);
+		w_m_i = -sin((2*PI)/m); 
+
+		for (k = 0; k < fft_length; k += m) {
+			// w = 1
+			w_r = 1;
+			w_i = 0;
+			for (j = 0; j < m/2; j++) {
+				// Real part
+				t_r = w_r*A_real[k + j + m/2] - w_i*A_imag[k + j + m/2];
+				u_r = A_real[k + j];
+				A_real[k + j] = u_r + t_r;
+				A_real[k + j + m/2] = u_r - t_r;
+				
+				// Imaginary part
+				t_i = w_r*A_imag[k + j + m/2] - w_i*A_real[k + j + m/2];
+				u_i = A_imag[k + j];
+				A_imag[k + j] = u_i + t_i;
+				A_imag[k + j + m/2] = u_i - t_i;
+				
+				// Complex multiplication to get w = w*w_m
+				w_r_temp = w_r; 										// Save a copy of the old w_r since it is used in both multiplications
+				w_r = w_r_temp*w_m_r - w_i*w_m_i; 
+        w_i = w_i*w_m_r 		 + w_r_temp*w_m_i;
+			}
+		}
+	}
+	
+	return 0;
+}
+
+void transformTask(void *params) {
+	uint16_t* output;
+	uint16_t input[4] = {2, 1, 0, 1};
+	volatile uint16_t bits;
+	volatile uint16_t result;
+	
+	bits = 0;
+	for (;;) {
 		
+		output = FFT(input, 4);
+		
+		// Do FFT
+		/*if (buffer_select == 1) {
+			FFT(audio_buffer_1) 
+		}
+		else {
+			FFT(audio_buffer_2);
+		}*/
+
+		// Absoulute value of result maybe
+		vTaskDelay(10 / portTICK_RATE_MS);
 	}
 }
 
 void setupFFT(unsigned portBASE_TYPE uxPriority) {
+	STM32_AudioRec_Init(SAMPLE_RATE_44100, DEFAULT_IN_BIT_RESOLUTION, DEFAULT_IN_CHANNEL_NBR);
+	STM32_AudioRec_Start((uint8_t*)audio_buffer_1, FFT_LENGTH);
   xTaskCreate(transformTask, "fft", 100, NULL, uxPriority, NULL);
 }
