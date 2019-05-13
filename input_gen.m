2c31faabf7004b3f13c73d5a558a91879ef42cd4
Jakob Nyberg <jakobnnyberg@gmail.com>
2019-05-13 14:37:15 +0200

    added expected output to generation

--- input_gen.m
+++ input_gen.m
@@ -1,7 +1,7 @@
 
-size = 16;
+size = 64;
 x = linspace(0, 2*pi, size);
-input =ones(1,16);
+input = sin(x);
 output = fft(input);
 in_real = real(input);
 in_imag = imag(input);
@@ -13,9 +13,11 @@ in_real_array = vec_to_c_array(in_real);
 in_imag_array = vec_to_c_array(in_imag);
 out_real_array =  vec_to_c_array(out_real);
 out_imag_array = vec_to_c_array(out_imag);
+magnitude_array = vec_to_c_array(abs(output));
 
 
-disp(sprintf("double input_real[%d] = ", size) + in_real_array + ";");
-disp(sprintf("double input_imag[%d] = ", size) + in_imag_array + ";");
-disp(sprintf("double expected_output_real[%d] = ", size) + out_real_array + ";");
-disp(sprintf("double expected_output_imag[%d] = ", size) + out_imag_array + ";");
+disp(sprintf("float input_real[%d] = ", size) + in_real_array + ";");
+disp(sprintf("float input_imag[%d] = ", size) + in_imag_array + ";");
+disp(sprintf("float expected_output_real[%d] = ", size) + out_real_array + ";");
+disp(sprintf("float expected_output_imag[%d] = ", size) + out_imag_array + ";");
+disp(sprintf("float expected_magnitude[%d] = ", size) + magnitude_array + ";");
