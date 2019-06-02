fda080aeea5998a1b58c3a8ec9c89c52e25a90cc
Unknown <magnus.ja.gustafsson@gmail.com>
2019-06-02 14:19:28 +0200

    Fixed indentation

--- skeleton-stm3220g/Project/STM32F2xx_StdPeriph_Template/touch.c
+++ skeleton-stm3220g/Project/STM32F2xx_StdPeriph_Template/touch.c
@@ -29,25 +29,25 @@ void touchTask(void* params){
   u8 pressed = 0;
   u8 i;
   for (;;) {
-    ts_state = IOE_TS_GetState();
-
-	if (pressed) {
-	  if (!ts_state->TouchDetected)
-	    pressed = 0;
-	} else if (ts_state->TouchDetected) {
-	  for (i = 0; i < callbacksNum; ++i) {
-		if (callback[i].left  <= ts_state->X &&
-		    callback[i].right >= ts_state->X &&
-		    callback[i].lower >= ts_state->Y &&
-		    callback[i].upper <= ts_state->Y){
-					callback[i].button.status = enable;
-					callback[i].func();
-				}	
-	  }													
-	  pressed = 1;
+		ts_state = IOE_TS_GetState();
+	
+		if (pressed) {
+			if (!ts_state->TouchDetected)
+				pressed = 0;
+		} else if (ts_state->TouchDetected) {
+			for (i = 0; i < callbacksNum; ++i) {
+			if (callback[i].left  <= ts_state->X &&
+					callback[i].right >= ts_state->X &&
+					callback[i].lower >= ts_state->Y &&
+					callback[i].upper <= ts_state->Y){
+						callback[i].button.status = enable;
+						callback[i].func();
+					}	
+			}													
+			pressed = 1;
+		}
+		vTaskDelayUntil( &lastWakeTime, 100 / portTICK_RATE_MS);
 	}
-	vTaskDelayUntil( &lastWakeTime, 100 / portTICK_RATE_MS);
-}
 }
 
 void selectPrevVisuliser() {
